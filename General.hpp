fcfdc953296b05631757d9a23d63cbcc00fc60ac
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 10:27:48 +0300

    Update General.hpp

--- General.hpp
+++ General.hpp
@@ -6,9 +6,18 @@
 
 namespace coup{
 	class General: public Player{
-		General(Game& game, string name);
-		~General();
-		void when_arrested() override;
+		public:
+			General(Game& game, string name);
+			~General();
+			//The General can undo a coup at the cost of 5 coins
+			//Returns true if they have enough (More than 5) and false otherwise
+			bool can_use_realtime_ability() const override;
+			//Subtracts 5 coins from the amount of coins they have
+			void realtime_ability() override;
+			//The General doesn't lose any coins when they are arrested. This method gets overriden to do nothing
+			void when_arrested() override;
+			//If the General is arrested, they don't lose any coins. Thus if they arrest is undoe, they don't get any either
+			void undo_arrest(bool arrested, Player* target) override;
 	};
 };
 
