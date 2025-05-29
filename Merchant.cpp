15e369592e84fe78b78c1fd1383fda125b2cce9d
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 10:32:05 +0300

    Update Merchant.cpp

--- Merchant.cpp
+++ Merchant.cpp
@@ -4,7 +4,9 @@
 
 using namespace coup;
 using namespace std;
+
 #define BAR_FOR_EXTRA_COIN 3
+#define ARREST_DEMAND 2
 
 Merchant::Merchant(Game& game, string name) : Player(name, "Merchant"){
     game.add_player(this);
@@ -16,11 +18,27 @@ void Merchant::turn_cycle_start(){
     if (coin_num >= BAR_FOR_EXTRA_COIN)
         coin_num++;
     
-    sanctioned = false;
-    arrest_blocked = false;
-
     if (extra_turns > 0) {
         extra_turns--;
     }
     
+}
+
+void Merchant::when_arrested(){
+    if (coin_num < ARREST_DEMAND){
+        throw "Too little coins for arrest";
+    }
+    change_coins(2, false);
+}
+
+void Merchant::undo_arrest(bool arrested, Player* target){
+    if (!target)
+        throw "Invalid argument: Null pointer";
+    if (arrested){
+        coin_num += ARREST_DEMAND;
+        return;
+    }
+    else if (target->role() == "Merchant")
+        return;
+    change_coins(1, false);
 }
