2ec145d8b3e99c8f4ae3bacb1ca11c295a387355
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 11:59:09 +0300

    Add files via upload

---
+++ Spy.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+#include "Spy.hpp"
+#include "Player.hpp"
+
+#define MUST_COUP 10
+
+using namespace std;
+using namespace coup;
+
+Spy::Spy(Game& game, string name) : Player(name, "Spy"){
+	game.add_player(this);
+}
+Spy::Spy(const Spy& spy) : Player(spy.name(), "Spy"){}
+
+Spy::~Spy(){}
+
+bool Spy::can_use_turn_ability() const{
+	return true;
+}
+
+bool Spy::can_use_realtime_ability() const{
+	return true;
+}
+
+size_t Spy::turn_ability(const Player* player){
+	if (player == nullptr)
+		throw "Invalid argument: Null player given";
+	if (player == this)
+		throw "Invliad argument: Cannot spy on yourself";
+	return player->coins();
+}
---
+++ Spy.hpp
@@ -0,0 +1,29 @@
+#ifndef SPY_HPP
+#define SPY_HPP
+
+#include "Game.hpp"
+#include "Player.hpp"
+
+namespace coup{
+	
+	class Spy: public Player{
+		public:
+			Spy(Game& game, string name);
+			//Important! Since the Game class doesn't allow for duplicate names, this copy constructor doesn't add the class to the game
+			Spy(const Spy& spy);
+			~Spy();
+			//Spy has a turn ability. Returns true.
+			bool can_use_turn_ability() const override;
+			//Spy has a realtime ability: When one Player wants to arrest another, Spy can undo the arrest.
+			//Prevention logic occurs at Game.cpp at prevent_action
+			//returns true
+			bool can_use_realtime_ability() const override;
+			//Spy's turn ability: They can spy on a target player and see how much coins the target has.
+			//Returns the number of coins the Player* player has
+			//Throws exepctions in case of the player being nullptr or this
+			size_t turn_ability(const Player* player) override;
+	};
+
+};
+
+#endif
