9fa3eba12db47e2be948b281c66980d68dac2fb4
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 10:28:19 +0300

    Update General.cpp

--- General.cpp
+++ General.cpp
@@ -2,6 +2,7 @@
 
 #include "General.hpp"
 
+#define COUP_PREVENT_PRICE 5
 using namespace std;
 using namespace coup;
 
@@ -11,6 +12,27 @@ General::General(Game& game, string name) : Player(name, "General"){
 
 General::~General(){}
 
+bool General::can_use_realtime_ability() const{
+    
+    return coin_num >= COUP_PREVENT_PRICE;
+}
+
+void General::realtime_ability(){
+    if (!can_use_realtime_ability())
+        throw "Invalid action. General must have 5 coins or more to prevent a coup";
+    coin_num -= COUP_PREVENT_PRICE;
+}
+
 void General::when_arrested(){
     return;
+}
+
+void General::undo_arrest(bool arrested, Player* target){
+    if (!target)
+        throw "Invalid argument: Null pointer";
+    if (arrested)
+        return;
+    if (target->role() == "Merchant")
+        return;
+    change_coins(1, false);
 }
