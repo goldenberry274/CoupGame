8fc0ef3eb4586ac5c0ffbaeb2b7c40ad8f512764
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 10:33:10 +0300

    Update Merchant.hpp

--- Merchant.hpp
+++ Merchant.hpp
@@ -6,9 +6,16 @@
 namespace coup{
 	
 	class Merchant: public Player{
-		Merchant(Game& game, string name);
-		~Merchant();
-		void turn_cycle_start() override;
+		public:
+			Merchant(Game& game, string name);
+			~Merchant();
+			//If the Merchant has more than 3 coins, they get 1 coin for free at the start of their turn
+			void turn_cycle_start() override;
+			//When the Merchant is arrested, they lost 2 coins, but don't give any to the arrester
+			void when_arrested() override;
+			//if arrested = true: Pays back the 2 coins the Merchant lost
+			//if arrested = false: Subtracts the coins they took from the other player
+			void undo_arrest(bool arrested, Player* target) override;
 	};
 
 };
