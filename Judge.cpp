632adcfeccaf74f96ae789e7a54428e6b3c7064f
Eitan Beriy <eit.beriy@gmail.com>
2025-05-29 12:05:00 +0300

    Update Judge.cpp

--- Judge.cpp
+++ Judge.cpp
@@ -9,6 +9,7 @@ Judge::Judge(Game& game, string name) : Player(name, "Judge"){
 	game.add_player(this);
 }
 
+Judge::Judge(const Judge& copy) : Player(copy.name(), "Judge"){}
 Judge::~Judge(){}
 
 bool Judge::can_use_realtime_ability() const{
