c09f58a416aad0e11b4317b775eaf26571b23cf6
Eitan Beriy <eit.beriy@gmail.com>
2025-05-30 12:22:16 +0300

    Update Baron.cpp

--- Baron.cpp
+++ Baron.cpp
@@ -13,7 +13,22 @@ Baron::Baron(Game& game, string name) : Player(name, "Baron"){
 	game.add_player(this);
 }
 
+Baron::Baron(const Baron& copy) : Player(copy.name(), "Baron"){
+    coin_num = copy.coins();
+    sanctioned = copy.is_sanctioned();
+    last_arrest = copy.last_arrest;
+    extra_turns = copy.extra_turns;
+	is_alive = copy.alive();
+}
 Baron::~Baron(){}
+
+Baron::Baron(const Player& copy) : Player(copy.name(), "Baron"){
+	coin_num = copy.coins();
+    sanctioned = copy.is_sanctioned();
+    last_arrest = copy.last_player_arrested();
+    extra_turns = copy.get_extra_turns();
+	is_alive = copy.alive();
+}
 
 bool Baron::can_use_turn_ability() const{
     
