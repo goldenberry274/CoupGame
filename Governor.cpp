47bace47e6ad5ef2fc85d77a17b6ed039562acf5
Eitan Beriy <eit.beriy@gmail.com>
2025-05-30 12:30:40 +0300

    Update Governor.cpp

--- Governor.cpp
+++ Governor.cpp
@@ -13,8 +13,23 @@ Governor::Governor(Game& game, string name) : Player(name, "Governor"){
 	game.add_player(this);
 }
 
+Governor::Governor(const Governor& copy) : Player(copy.name(), "Governor"){
+    coin_num = copy.coins();
+    sanctioned = copy.is_sanctioned();
+    last_arrest = copy.last_arrest;
+    extra_turns = copy.extra_turns;
+	is_alive = copy.alive();
+}
+Governor::Governor(const Player& copy) : Player(copy.name(), "Governor"){
+	coin_num = copy.coins();
+    sanctioned = copy.is_sanctioned();
+    last_arrest = copy.last_player_arrested();
+    extra_turns = copy.get_extra_turns();
+	is_alive = copy.alive();
+}
 
 Governor::~Governor(){}
+
 
 void Governor::tax() {
 	coin_num += TAX_PAYMENT;
